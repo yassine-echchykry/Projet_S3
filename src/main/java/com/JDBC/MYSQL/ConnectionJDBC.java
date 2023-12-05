0e90605f3e1779317380a499d1b4932345ad91b2
Yass1509 <yechchykry@gmail.com>
2023-12-05 15:29:52 +0100

    DAO2

--- src/main/java/com/JDBC/MYSQL/ConnectionJDBC.java
+++ src/main/java/com/JDBC/MYSQL/ConnectionJDBC.java
@@ -32,7 +32,7 @@ public class ConnectionJDBC {
 		        }
 
 		        /* Connexion à la base de données */
-		        String url = "jdbc:mysql://localhost:3306/dbb_KMO";
+		        String url = "jdbc:mysql://localhost:3306/picalti";
 		        String utilisateur = "root";
 		        String motDePasse = "";
 		        Connection connexion = null;
